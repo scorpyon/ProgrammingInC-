2dcad4925adc836f32fd8c9a86ac0c0fa46817b3
Danny Jeffery <daniel.jeffery@globalgraphics.com>
2020-01-10 09:49:54 +0000

    First Commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProgrammingInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program started.");
+            Console.WriteLine("");
+            Console.WriteLine("...");
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to end program.");
+            Console.ReadKey();
+        }
+    }
+}
